7bdf360c6b95de8b0f79cea9dac077b9d7f67226
riski maulana rahman <riskimaulanarahman@gmail.com>
2021-08-19 23:54:17 +0800

    change name backend

--- resources/views/layouts/Backend.blade.php
+++ resources/views/layouts/Backend.blade.php
@@ -14,12 +14,13 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend.css') }}">
     <!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}"> -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
-    
+
     <!-- <script src="{{ asset('assets/scripts/app.js')}}"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> -->
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    {{-- backend --}}
 
 </head>
 <body>
